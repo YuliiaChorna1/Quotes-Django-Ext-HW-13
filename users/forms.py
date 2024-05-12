10a4417410d85c236ad436ed1c269d61a8085912
Yuliia Chorna <140984907+YuliiaChorna1@users.noreply.github.com>
2024-05-12 12:25:45 +1000

    Fixed bug with missing email

--- users/forms.py
+++ users/forms.py
@@ -21,7 +21,7 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
 
 
 class LoginForm(AuthenticationForm):
